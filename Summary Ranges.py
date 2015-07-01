b587cd2afc7876c158535d39d964b0b3cfbd9651
Geek Cheng <chengzhoukun@gmail.com>
2015-07-01 22:19:21 +0800

    Summary Ranges

--- Summary Ranges.py
+++ Summary Ranges.py
@@ -4,7 +4,7 @@ class Solution:
     def summaryRanges(self, nums):
         start = 0
         ans = []
-        nums.append(0)
+        nums.append(99999999999999999)
         for i in range(0, len(nums)):
             if nums[i] - nums[start] != i - start:
                 if i - start > 1:
@@ -15,4 +15,4 @@ class Solution:
                 start = i
         return ans
 
-print(Solution().summaryRanges([-1]))
+# print(Solution().summaryRanges([-1, 0, 1, 2, 4, 5, 7]))
