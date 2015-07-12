3396d7c3f7ec9298df94190df7560d3e7c16b7c0
chengzhoukun <chengzhoukun@gmail.com>
2015-07-12 23:53:42 +0800

    Implement Trie (Prefix Tree)

---
+++ Add and Search Word - Data structure design.py
@@ -0,0 +1,19 @@
+class WordDictionary:
+    # initialize your data structure here.
+    def __init__(self):
+
+    # @param {string} word
+    # @return {void}
+    # Adds a word into the data structure.
+    def addWord(self, word):
+
+    # @param {string} word
+    # @return {boolean}
+    # Returns if the word is in the data structure. A word could
+    # contain the dot character '.' to represent any one letter.
+    def search(self, word):
+
+    # Your WordDictionary object will be instantiated and called as such:
+    # wordDictionary = WordDictionary()
+    # wordDictionary.addWord("word")
+    # wordDictionary.search("pattern")
---
+++ Implement Trie (Prefix Tree).py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+
+class TrieNode:
+    # Initialize your data structure here.
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.isWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    # @param {string} word
+    # @return {void}
+    # Inserts a word into the trie.
+    def insert(self, word):
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+        current.isWord = True
+
+    # @param {string} word
+    # @return {boolean}
+    # Returns if the word is in the trie.
+    def search(self, word):
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return current.isWord
+
+    # @param {string} prefix
+    # @return {boolean}
+    # Returns if there is any word in the trie
+    # that starts with the given prefix.
+    def startsWith(self, prefix):
+        current = self.root
+        for letter in prefix:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# trie = Trie()
+# trie.insert("somestring")
+# trie.search("key")
