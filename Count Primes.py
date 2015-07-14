6b0041040743b47178110cf9291d4b42fab5b9fb
chengzhoukun <chengzhoukun@gmail.com>
2015-07-15 00:27:49 +0800

    Count Primes

---
+++ Count Primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+        isPrime = [True] * n
+        isPrime[0] = isPrime[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if isPrime[i]:
+                isPrime[i * i: n: i] = [False] * len(isPrime[i * i: n: i])
+        return sum(isPrime)
