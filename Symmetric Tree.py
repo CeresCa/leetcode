294d3c3ab623f7e83755bf515781db33f1c46819
chengzhoukun <chengzhoukun@gmail.com>
2015-07-18 13:55:12 +0800

    complete the Symmetric TRee

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -9,3 +9,17 @@ class Solution:
     # @param {TreeNode} root
     # @return {boolean}
     def isSymmetric(self, root):
+        if root is None:
+            return True
+        else:
+            return self.isMirrror(root.left, root.right)
+
+    def isMirrror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.val != right.val:
+            return False
+        else:
+            return self.isMirrror(left.left, right.right) and self.isMirrror(left.right, right.left)
