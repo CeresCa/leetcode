38bb015cd101f33939a3b7b179a31306567045d2
chengzhoukun <chengzhoukun@gmail.com>
2015-07-22 02:34:57 +0800

    Isomorphic Strings

---
+++ Isomorphic Strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param {string} s
+    # @param {string} t
+    # @return {boolean}
+    def isIsomorphic(self, s, t):
+        sourceMap, targetMap = dict(), dict()
+        for i in range(len(s)):
+            source, target = sourceMap.get(t[i]), targetMap.get(s[i])  # use dict().get() instead of index
+            if source is None and target is None:
+                sourceMap[t[i]], targetMap[s[i]] = s[i], t[i]
+            elif source != s[i] or target != t[i]:
+                return False
+        return True
+
+    # short version:
+    # def isIsomorphic(self, s, t):
+    # return len(set(zip(s, t))) == len(set(s)) == len(set(t))
