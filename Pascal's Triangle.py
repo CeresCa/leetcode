0e35e46ebdef501fe16f11f1dd9267934166036c
chengzhoukun <chengzhoukun@gmail.com>
2015-08-03 11:29:32 +0800

    Pascal's Triangle

---
+++ Pascal's Triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {integer} numRows
+    # @return {integer[][]}
+
+    def generate(self, numRows):
+        lists = []
+        for i in range(numRows):
+            lists.append([1] * (i + 1))
+            if i > 1:
+                for j in range(1, i):
+                    lists[i][j] = lists[i - 1][j - 1] + lists[i - 1][j]
+        return lists
