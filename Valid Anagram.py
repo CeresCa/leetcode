4384c14b3435b3a9bf8705689673ff1ce231e6e8
chengzhoukun <chengzhoukun@gmail.com>
2015-08-04 01:31:43 +0800

    add a solution using array

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -4,5 +4,18 @@ class Solution:
     # @return {boolean}
 
     # use sorting
+    # def isAnagram(self, s, t):
+    #     return sorted(s) == sorted(t)
+
+    # use array
     def isAnagram(self, s, t):
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        counts = [0 for i in range(26)]
+        for i in range(0, len(s)):
+            counts[ord(s[i]) - ord('a')] += 1
+            counts[ord(t[i]) - ord('a')] -= 1
+        for i in range(0, 26):
+            if counts[i]:
+                return False
+        return True
