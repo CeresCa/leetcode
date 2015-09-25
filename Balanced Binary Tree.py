d0a8be40f868cfcf9ad18a3226a71bca57e9b606
chengzhoukun <chengzhoukun@gmail.com>
2015-09-25 11:31:28 +0800

    update

--- Balanced Binary Tree.py
+++ Balanced Binary Tree.py
@@ -5,16 +5,18 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     # @param {TreeNode} root
     # @return {boolean}
+
     def height(self, root):
-        if root == None:
+        if root is None:
             return 0
         return max(self.height(root.left), self.height(root.right)) + 1
 
     def isBalanced(self, root):
-        if root == None:
+        if root is None:
             return True
         if abs(self.height(root.left) - self.height(root.right)) <= 1:
             return self.isBalanced(root.left) and self.isBalanced(root.right)
